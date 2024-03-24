caef58022aacd329fe3dc5dab0ebaaaa341ec866
Varos1009 <85431234+Varos1009@users.noreply.github.com>
2024-03-24 21:02:30 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let str = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        str =
+          pronoun[Math.round(Math.random())] +
+          adj[Math.round(Math.random())] +
+          noun[Math.round(Math.random())] +
+          ".com";
+      }
+    }
+  }
+  document.getElementById("generator").innerHTML = str;
+  console.log(str);
 };
